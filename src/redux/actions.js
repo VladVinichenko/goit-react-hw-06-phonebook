a0d7ae17fbaacba85ab7802f0e961d16c63c20b1
VladVinichenko <vinnyxxxl925@gmail.com>
2022-01-29 07:02:31 +0200

    c

---
+++ src/redux/actions.js
@@ -0,0 +1,11 @@
+export const myAction = {
+  type: 'MY_ACTION',
+  payload: 'super payload',
+}
+
+export const myAction = value => {
+  return {
+    type: 'MY_ACTION',
+    payload: value,
+  }
+}
